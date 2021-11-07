e99c67d0582b4ff422f89e7dad4f0369e244eb8a
kkojae91 <jaejeung3210@gmail.com>
2021-11-07 13:51:04 +0900

    js와 jsx 섞어쓰기- key 추가

--- basic-react/01ch/js와jsx섞어쓰기.js
+++ basic-react/01ch/js와jsx섞어쓰기.js
@@ -19,7 +19,7 @@ const element = (
     <Text text="Text"></Text>
     <Text text="text"></Text>
     {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((number) => (
-      <Num number={number} selected={number === 3} />
+      <Num number={number} key={number} selected={number === 3} />
     ))}
   </>
 );
